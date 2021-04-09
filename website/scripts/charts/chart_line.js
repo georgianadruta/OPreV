4d557d9c841027d367d236d83c4e08b930507a58
Martonca Daniel <martonca.daniel@yahoo.com>
2021-04-09 16:29:54 +0300

    Added Obese and PreObese data to line chart.

--- website/scripts/charts/chart_line.js
+++ website/scripts/charts/chart_line.js
@@ -1,8 +1,15 @@
 const data_labels = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2020", "2021"];
-const dataset2008 = [NaN, NaN, NaN, NaN, 51.1, 51.8, 51.8, 51.8];
-const dataset2014 = [NaN, NaN, NaN, NaN, 55.1, 41.8, 51.8, 91.8];
-const dataset2017 = [NaN, NaN, NaN, NaN, 55.1, 51.8, 61.8, 21.8];
+const Overweight_dataset2008 = [NaN, NaN, NaN, NaN, 51.1, 51.8, 51.8, 51.8];
+const Overweight_dataset2014 = [NaN, NaN, NaN, NaN, 55.1, 41.8, 51.8, 91.8];
+const Overweight_dataset2017 = [NaN, NaN, NaN, NaN, 55.1, 51.8, 61.8, 21.8];
 
+const PreObese_dataset2008 = [NaN, NaN, NaN, NaN, 41.1, 55.8, 50.8, 59.8];
+const PreObese_dataset2014 = [NaN, NaN, NaN, NaN, 52.1, 43.8, 41.8, 51.8];
+const PreObese_dataset2017 = [NaN, NaN, NaN, NaN, 20.1, 21.8, 31.8, 27.8];
+
+const Obese_dataset2008 = [NaN, NaN, NaN, NaN, 21.1, 21.8, 31.8, 41.8];
+const Obese_dataset2014 = [NaN, NaN, NaN, NaN, 25.1, 21.8, 31.8, 41.8];
+const Obese_dataset2017 = [NaN, NaN, NaN, NaN, 25.1, 21.8, 31.8, 41.8];
 
 var data = {
     labels: data_labels,
@@ -11,8 +18,23 @@ var data = {
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0,
-        data: dataset2008
-    }]
+        data: Overweight_dataset2008
+    },
+    {
+        label: 'PreObese BMI',
+        fill: false,
+        borderColor: 'red',
+        tension: 0,
+        data: PreObese_dataset2008
+    },
+    {
+        label: 'Obese by BMI',
+        fill: false,
+        borderColor: 'black',
+        tension: 0,
+        data: Obese_dataset2008
+    },
+    ]
 };
 
 var options = {
@@ -37,15 +59,21 @@ window.onload = function () {
 function changeChartDataByYear(year) {
     switch (year) {
         case '2008': {
-            lineChart.data.datasets[0].data = dataset2008;
+            lineChart.data.datasets[0].data = Overweight_dataset2008;
+            lineChart.data.datasets[1].data = PreObese_dataset2008;
+            lineChart.data.datasets[2].data = Obese_dataset2008;
             break;
         }
         case '2014': {
-            lineChart.data.datasets[0].data = dataset2014;
+            lineChart.data.datasets[0].data = Overweight_dataset2014;
+            lineChart.data.datasets[1].data = PreObese_dataset2014;
+            lineChart.data.datasets[2].data = Obese_dataset2014;
             break;
         }
         case '2017': {
-            lineChart.data.datasets[0].data = dataset2017;
+            lineChart.data.datasets[0].data = Overweight_dataset2017;
+            lineChart.data.datasets[1].data = PreObese_dataset2017;
+            lineChart.data.datasets[2].data = Obese_dataset2017;
             break;
         }
     }
