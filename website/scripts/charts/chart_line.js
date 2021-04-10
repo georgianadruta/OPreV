b9ef6ca7876161eaa74b4eb7064e2296ee34724f
Martonca Daniel <martonca.daniel@yahoo.com>
2021-04-10 13:55:11 +0300

    modified line chart and third page strucutre

--- website/scripts/charts/chart_line.js
+++ website/scripts/charts/chart_line.js
@@ -1,38 +1,44 @@
-const data_labels = ["2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2020", "2021"];
-const Overweight_dataset2008 = [NaN, NaN, NaN, NaN, 51.1, 51.8, 51.8, 51.8];
-const Overweight_dataset2014 = [NaN, NaN, NaN, NaN, 55.1, 41.8, 51.8, 91.8];
-const Overweight_dataset2017 = [NaN, NaN, NaN, NaN, 55.1, 51.8, 61.8, 21.8];
+const data_labels = ["2010", "2012", "2014", "2016", "2018", "2020", "2021-present"]
 
-const PreObese_dataset2008 = [NaN, NaN, NaN, NaN, 41.1, 55.8, 50.8, 59.8];
-const PreObese_dataset2014 = [NaN, NaN, NaN, NaN, 52.1, 43.8, 41.8, 51.8];
-const PreObese_dataset2017 = [NaN, NaN, NaN, NaN, 20.1, 21.8, 31.8, 27.8];
+const Overweight_dataset1 = [NaN, NaN, NaN, 51.1, NaN, 51.8, NaN];
+const PreObese_dataset1 = [NaN, NaN, NaN, 35.7, NaN, 36.9, NaN];
+const Obese_dataset1 = [NaN, NaN, NaN, 15.4, NaN, 14.6, NaN];
 
-const Obese_dataset2008 = [NaN, NaN, NaN, NaN, 21.1, 21.8, 31.8, 41.8];
-const Obese_dataset2014 = [NaN, NaN, NaN, NaN, 25.1, 21.8, 31.8, 41.8];
-const Obese_dataset2017 = [NaN, NaN, NaN, NaN, 25.1, 21.8, 31.8, 41.8];
+const Overweight_dataset2 = [NaN, NaN, NaN, 21.1, NaN, 41.8, NaN];
+const PreObese_dataset2 = [NaN, NaN, NaN, 31.1, NaN, 68.8, NaN];
+const Obese_dataset2 = [NaN, NaN, NaN, 11.1, NaN, 62.8, 65.5];
+
 
 var data = {
     labels: data_labels,
     datasets: [{
         label: 'Overweight by BMI',
+        data: Overweight_dataset1,
+
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0,
-        data: Overweight_dataset2008
+        spanGaps: true,
+        pointHoverBorderColor: "black",
+        pointHoverRadius: 7
     },
     {
         label: 'PreObese BMI',
+        data: PreObese_dataset1,
+
         fill: false,
         borderColor: 'red',
         tension: 0,
-        data: PreObese_dataset2008
+        spanGaps: true
     },
     {
         label: 'Obese by BMI',
+        data: Obese_dataset1,
+
         fill: false,
         borderColor: 'black',
         tension: 0,
-        data: Obese_dataset2008
+        spanGaps: true
     },
     ]
 };
@@ -56,24 +62,18 @@ window.onload = function () {
     lineChart = new Chart(document.getElementById('lineChart').getContext('2d'), config);
 };
 
-function changeChartDataByYear(year) {
-    switch (year) {
-        case '2008': {
-            lineChart.data.datasets[0].data = Overweight_dataset2008;
-            lineChart.data.datasets[1].data = PreObese_dataset2008;
-            lineChart.data.datasets[2].data = Obese_dataset2008;
+function changeChartData(data_origin) {
+    switch (data_origin) {
+        case 'European Union - 27 countries (from 2020)': {
+            lineChart.data.datasets[0].data = Overweight_dataset1;
+            lineChart.data.datasets[1].data = PreObese_dataset1;
+            lineChart.data.datasets[2].data = Obese_dataset1;
             break;
         }
-        case '2014': {
-            lineChart.data.datasets[0].data = Overweight_dataset2014;
-            lineChart.data.datasets[1].data = PreObese_dataset2014;
-            lineChart.data.datasets[2].data = Obese_dataset2014;
-            break;
-        }
-        case '2017': {
-            lineChart.data.datasets[0].data = Overweight_dataset2017;
-            lineChart.data.datasets[1].data = PreObese_dataset2017;
-            lineChart.data.datasets[2].data = Obese_dataset2017;
+        case 'Euro area - 19 countries  (from 2015)': {
+            lineChart.data.datasets[0].data = Overweight_dataset2;
+            lineChart.data.datasets[1].data = PreObese_dataset2;
+            lineChart.data.datasets[2].data = Obese_dataset2;
             break;
         }
     }
