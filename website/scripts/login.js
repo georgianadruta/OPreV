67a235503cb5c829ec387ef25e6e59819addbffa
drtgeorgiana <drtgeorgiana@gmail.com>
2021-04-11 18:17:14 +0300

    added toggle password visibilty in login form

--- website/scripts/login.js
+++ website/scripts/login.js
@@ -1,8 +1,9 @@
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+function myFunction() {
+    var x = document.getElementById("myInput");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+    this.classList.toggle('fa-eye-slash');
 }
