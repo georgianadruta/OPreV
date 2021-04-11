f560d85fd3b952be9b26a22b3c754dbbd4c374c4
Martonca Daniel <martonca.daniel@yahoo.com>
2021-04-11 19:31:55 +0300

    fixed side nav bar missing items

---
+++ website/scripts/admin.js
@@ -0,0 +1,63 @@
+const countries = ["Albania",
+    "Andorra",
+    "Armenia",
+    "Austria",
+    "Azerbaijan",
+    "Belarus",
+    "Belgium",
+    "Bosnia and Herzegovina",
+    "Bulgaria",
+    "Croatia",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Estonia",
+    "Finland",
+    "France",
+    "Georgia",
+    "Germany",
+    "Greece",
+    "Hungary",
+    "Iceland",
+    "Ireland",
+    "Italy",
+    "Kazakhstan",
+    "Latvia",
+    "Liechtenstein",
+    "Lithuania",
+    "Luxembourg",
+    "Macedonia",
+    "Malta",
+    "Moldova",
+    "Monaco",
+    "Montenegro",
+    "Netherlands",
+    "Norway",
+    "Poland",
+    "Portugal",
+    "Romania",
+    "Russia",
+    "San Marino",
+    "Serbia",
+    "Slovakia",
+    "Slovenia",
+    "Spain",
+    "Sweden",
+    "Switzerland",
+    "Turkey",
+    "Ukraine",
+    "United Kingdom",
+    "Vatican City",];
+
+var select;
+
+function addOption(item, index) {
+    var option = document.createElement("option");
+    option.text = item;
+    select.add(option);
+}
+
+window.onload = function () {
+    select = document.getElementById("country");
+    countries.forEach(addOption);
+}
