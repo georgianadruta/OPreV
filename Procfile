d38becf2bed466a73bf6bb1a59f0334e6f2b4a95
NarcisSt <75793612+NarcisSt@users.noreply.github.com>
2021-05-18 20:36:41 +0300

    Refactored the pages and change login/register and some styles

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: node REST-API/api.js
--- REST-API/api.js
+++ REST-API/api.js
@@ -1,74 +1,47 @@
+const PORT = 8125;
 const http = require('http');
-const {URL} = require('url');
 const fs = require('fs');
 const path = require('path');
-const isUrlValid = require('valid-url');
-
-const PORT = process.env.PORT || 8081;
-const mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.ico': 'image/x-icon',
-};
-
-let datasetPath = "/dataset";
-
 
 http.createServer(function (request, response) {
 
     console.log(request.method, request.url);
 
-    //check if url is valid
-    if (isUrlValid.isUri(request.url)) {
-        const myURL = new URL(request.url);
-        if (myURL.pathname === datasetPath)
-            switch (request.method) {
-                case "GET": {
-                    //TODO: GET logic
-                    return;
-                }
-                case "POST": {
-                    //TODO: POST logic
-                    return;
-                }
-                case "PUT": {
-                    //TODO: PUT logic
-                    return;
-                }
-                case "DELETE": {
-                    //TODO: DELETE logic
-                    return;
-                }
-            }
-    }
-
-    //create file path
     let filePath = '.' + request.url;
-    if (filePath === './')
-        filePath = './OPreV.html';
-
-    try {
-        if (fs.existsSync(filePath)) {
-            //check if it's valid
-            const extname = String(path.extname(filePath)).toLowerCase();
-            const contentType = mimeTypes[extname] || 'application/octet-stream';// octet-stream is the default value if no mimeTypes is found
-            fs.readFile(filePath, function (error, content) {
-                if (error) {
-                    console.log("ERROR reading the file: " + filePath);
-                    response.writeHead(404, {'Content-Type': contentType});
-                    response.end(content, 'utf-8');
-                } else {
-                    response.writeHead(200, {'Content-Type': contentType});
-                    response.end(content, 'utf-8');
-                }
-            });
-        }
-    } catch (err) {
-        console.error(err)
+    if (filePath === './') {
+        filePath = './/';
     }
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const mimeTypes = {
+        '.html': 'text/html',
+        '.js': 'text/javascript',
+        '.css': 'text/css',
+        '.json': 'application/json',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.ico': 'image/x-icon',
+    };
+
+    const contentType = mimeTypes[extname] || 'application/octet-stream';// octet-stream is the default value if no mimeTypes is found
+
+    fs.readFile(filePath, function (error, content) {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                fs.readFile('./404.html', function (error, content) {
+                    response.writeHead(404, {'Content-Type': 'text/html'});
+                    response.end(content, 'utf-8');
+                });
+            } else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: ' + error.code + ' ..\n');
+            }
+        } else {
+            response.writeHead(200, {'Content-Type': contentType});
+            response.end(content, 'utf-8');
+        }
+    });
+
 }).listen(PORT);
+// }).listen(process.env.PORT || port);
 console.log('Server running at http://127.0.0.1:' + PORT);
