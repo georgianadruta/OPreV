d492293bb9801548e237ca728e63a525716726af
Martonca Daniel <martonca.daniel@yahoo.com>
2021-05-20 22:49:21 +0300

    Added setCookie and getCookie methods

---
+++ website/scripts/BACK-END/cookies.js
@@ -0,0 +1,32 @@
+/**
+ * This function's purpose is to create a new cookie with the parameters specified.
+ * @param cookieName the name of the cookie
+ * @param cookieValue the value of the cooke
+ * @param expireMinutes an expiration date on minutes. Default no expiry date (session cookie)
+ */
+function setCookie(cookieName, cookieValue, expireMinutes = null) {
+    const d = new Date();
+    d.setTime(d.getTime() + (expireMinutes * 60 * 60 * 1000));
+    const expires = "expires=" + d.toUTCString();
+    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+/**
+ * This function's purpose is to return a string with the cookie named cookieName or null if no cookie exists with that name
+ * @param cookieName the name of the cookie
+ * @returns {string|null} a string containing the cookie OR null if no cookies exists with that name
+ */
+function getCookie(cookieName) {
+    const name = cookieName + "=";
+    const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return null;
+}
