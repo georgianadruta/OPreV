b6d36d004b9b9da81ecd359f050464651fcf208c
drtgeorgiana <drtgeorgiana@gmail.com>
2021-05-27 16:17:08 +0300

    added HTTP request for countries

--- website/scripts/BACK-END/data.js
+++ website/scripts/BACK-END/data.js
@@ -102,7 +102,17 @@ async function getAllPossibleValuesOfFilter(filterName) {
  * @returns {string[]} array of strings each representing a label
  */
 async function getCountriesHTTPRequest() {
-
+    if (getCookie("dataset").toLowerCase() === "eurostat") {
+        let countryList = Array();
+        for (let i = 0; i < tableInformation.dataset.length; i = i + 3) {
+            countryList.push(tableInformation.dataset[i].country);
+        }
+        return countryList;
+    } else {
+        if (getCookie("dataset").toLowerCase() === "who") {
+            // TODO
+        }
+    }
 }
 
 
