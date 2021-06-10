15f9b536a1b651a6f9489b32104746b0979a3c49
drtgeorgiana <drtgeorgiana@gmail.com>
2021-06-10 22:56:22 +0300

    added line chart support. need to fix deselect country

--- website/scripts/BACK-END/charts/chart_line.js
+++ website/scripts/BACK-END/charts/chart_line.js
@@ -2,17 +2,10 @@ class LineChart extends OPreVChart {
     chart = null;
     data = {
         labels: Array(),
-        datasets: [{
-            label: '',
-            data: this.tableInformation.dataset,
-            fill: false,
-            backgroundColor: 'rgb(39, 174, 96)',
-            borderColor: 'rgb(39, 174, 96)',
-            tension: 0.1
-        }]
+        datasets: []
     };
     config = {
-        type: 'line',
+        type: "line",
         data: this.data,
         options: this.options,
     };
@@ -34,7 +27,67 @@ class LineChart extends OPreVChart {
     }
 
     generateLabels() {
-        this.data.labels = window.sessionStorage.getItem("years").split(',');
+        this.data.labels = window.sessionStorage.getItem("years").split(',').sort();
+    }
+
+    addToDataset(row) {
+        let ok = false;
+        this.data.datasets.forEach(json => {
+            if (ok === true) {
+                return;
+            }
+            if (json.label === row.country) {
+                json.data.push(row.BMI_value);
+                ok = true;
+            }
+        });
+        if (ok === false) {
+            function random_rgba() {
+                let o = Math.round, r = Math.random, s = 255;
+                return 'rgba(' + o(r() * s) + ',' + o(r() * s) + ',' + o(r() * s) + ',' + r().toFixed(1) + ')';
+            }
+
+            let json = {
+                label: row.country,
+                data: [row.BMI_value],
+                fill: false,
+                backgroundColor: random_rgba(),
+                borderColor: random_rgba(),
+                tension: 0.1
+            };
+            this.data.datasets.push(json);
+        }
+    }
+
+    /**
+     * This function's purpose is to sort dataset by country name and year
+     */
+    sortDatasetByYear() {
+        this.tableInformation.dataset.sort((a, b) => {
+                if (a["country"].localeCompare(b["country"]) === 0) {
+                    return a["year"].localeCompare(b["year"]);
+                } else {
+                    return a["country"].localeCompare(b["country"]);
+                }
+            }
+        );
+    }
+
+    async generateData() {
+        try {
+            await getDatasetHTTPRequest().then(result => {
+                if (result !== null) {
+                    this.tableInformation = result;
+                    this.sortDatasetByYear();
+                    this.data.datasets = [];
+                    this.tableInformation.dataset.forEach(row => {
+                        this.addToDataset(row);
+                    });
+                }
+            });
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     /**
@@ -42,10 +95,12 @@ class LineChart extends OPreVChart {
      */
     generateLineChart() {
         this.generateLabels();
-        if (this.chart !== null) {
-            this.chart.destroy();
-        }
-        this.chart = new Chart(document.getElementById('lineChart'), this.config);
+        this.generateData().then(() => {
+            if (this.chart !== null) {
+                this.chart.destroy();
+            }
+            this.chart = new Chart(document.getElementById("lineChart"), this.config);
+        });
     }
 }
 
